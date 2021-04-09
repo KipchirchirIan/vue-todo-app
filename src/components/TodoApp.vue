2765ebae42ebb37e3e5c0ee18c6c1517ee136760
Ian Kipchirchir <potterke4@gmail.com>
2021-04-09 17:15:42 +0300

    Show a simple list of items

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-for="(todo, index) in todos" :key="index">
+      {{ todo }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoApp",
+  data: function () {
+    return {
+      todos: ["Learn Vue", "Learn Laravel", "Build a LaraVue project"],
+      completedTodos: [],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
